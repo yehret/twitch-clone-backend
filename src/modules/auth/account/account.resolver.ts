828d1a36b4cfa7713769cafa1906e0c19cac65e1
Serhii Samoliuk <samolyk2005@gmail.com>
2025-02-01 14:28:17 +0100

    authorization

--- src/modules/auth/account/account.resolver.ts
+++ src/modules/auth/account/account.resolver.ts
@@ -1,5 +1,8 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 
+import { Authorization } from '@/src/shared/decorators/auth.decorator'
+import { Authorized } from '@/src/shared/decorators/authorized.decorator'
+
 import { AccountService } from './account.service'
 import { CreateUserInput } from './inputs/create-user.input'
 import { UserModel } from './models/user.model'
@@ -8,9 +11,10 @@ import { UserModel } from './models/user.model'
 export class AccountResolver {
 	public constructor(private readonly accountService: AccountService) {}
 
-	@Query(() => [UserModel], { name: 'findAllUsers' })
-	public async findAll() {
-		return this.accountService.findAll()
+	@Authorization()
+	@Query(() => UserModel, { name: 'findProfile' })
+	public async me(@Authorized('id') id: string) {
+		return this.accountService.me(id)
 	}
 
 	@Mutation(() => Boolean, { name: 'createUser' })
--- src/modules/auth/account/account.service.ts
+++ src/modules/auth/account/account.service.ts
@@ -9,10 +9,14 @@ import { CreateUserInput } from './inputs/create-user.input'
 export class AccountService {
 	public constructor(private readonly prismaService: PrismaService) {}
 
-	public async findAll() {
-		const users = await this.prismaService.user.findMany()
+	public async me(id: string) {
+		const user = await this.prismaService.user.findUnique({
+			where: {
+				id
+			}
+		})
 
-		return users
+		return user
 	}
 
 	public async create(input: CreateUserInput) {
